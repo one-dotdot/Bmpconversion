d35b1adce359d27d96f7d5c92fbf92983c121031
Artie 陈步权 <artie_chen@intretech.com>
2023-07-05 21:07:28 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Bmpconversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.text_path = new System.Windows.Forms.TextBox();
+            this.btn_choose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.log = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "生成BIN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnConvert2_Click);
+            // 
+            // text_path
+            // 
+            this.text_path.Location = new System.Drawing.Point(9, 27);
+            this.text_path.Name = "text_path";
+            this.text_path.Size = new System.Drawing.Size(264, 25);
+            this.text_path.TabIndex = 1;
+            // 
+            // btn_choose
+            // 
+            this.btn_choose.Location = new System.Drawing.Point(279, 19);
+            this.btn_choose.Name = "btn_choose";
+            this.btn_choose.Size = new System.Drawing.Size(85, 37);
+            this.btn_choose.TabIndex = 6;
+            this.btn_choose.Text = "选择文件";
+            this.btn_choose.UseVisualStyleBackColor = true;
+            this.btn_choose.Click += new System.EventHandler(this.btn_choose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(219, 449);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(367, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(219, 449);
+            this.panel1.TabIndex = 8;
+            // 
+            // log
+            // 
+            this.log.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.log.Location = new System.Drawing.Point(9, 179);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(352, 258);
+            this.log.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 70);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "生成TXT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnConvertc_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 70);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "批量处理";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttonhex_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(259, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 70);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "BIN合成";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnConcatenate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 449);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_choose);
+            this.Controls.Add(this.text_path);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "图片转换";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text_path;
+        private System.Windows.Forms.Button btn_choose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Bmpconversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert2_Click(object sender, EventArgs e)
+        {
+            string bmpFilePath = text_path.Text;
+            string hexContent = BmpToHex(bmpFilePath);
+            string swappedHexContent = SwapHexContent(hexContent);
+
+            byte[] hexBytes = Enumerable.Range(0, swappedHexContent.Length / 2)
+                .Select(i => Convert.ToByte(swappedHexContent.Substring(i * 2, 2), 16))
+                .ToArray();
+
+            // Calculate CRC
+            ushort crc = CalculateCRC(hexBytes);
+
+            // Convert CRC to hex string
+            string crcHexString = crc.ToString("X4");
+
+            // Append CRC to hex content
+            string hexContentWithCRC = swappedHexContent + crcHexString;
+
+            // Convert hex content with CRC to bytes
+            byte[] hexBytesWithCRC = Enumerable.Range(0, hexContentWithCRC.Length / 2)
+                .Select(i => Convert.ToByte(hexContentWithCRC.Substring(i * 2, 2), 16))
+                .ToArray();
+
+            // 填充剩余的字节为0xFF
+            int paddingSize = 108 * 1024 - hexBytesWithCRC.Length;
+            byte[] paddingBytes = Enumerable.Repeat((byte)0xFF, paddingSize).ToArray();
+
+            // Concatenate hex bytes with CRC and padding bytes
+            byte[] finalHexBytes = hexBytesWithCRC.Concat(paddingBytes).ToArray();
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "BIN Files (*.bin)|*.bin";
+            saveFileDialog.InitialDirectory = Path.GetDirectoryName(bmpFilePath);
+            saveFileDialog.FileName = Path.GetFileNameWithoutExtension(bmpFilePath) + ".bin";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllBytes(saveFileDialog.FileName, finalHexBytes);
+            }
+        }
+
+        private ushort CalculateCRC(byte[] data)
+        {
+            ushort crc = 0xFFFF;
+
+            foreach (byte b in data)
+            {
+                crc ^= (ushort)(b << 8);
+
+                for (int i = 0; i < 8; i++)
+                {
+                    if ((crc & 0x8000) != 0)
+                    {
+                        crc = (ushort)((crc << 1) ^ 0x1021);
+                    }
+                    else
+                    {
+                        crc <<= 1;
+                    }
+                }
+            }
+
+            return crc;
+        }
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertc_Click(object sender, EventArgs e)
+        {
+            string bmpFilePath = text_path.Text;
+            string hexContent = BmpToHex(bmpFilePath);
+            string swappedHexContent = SwapHexContent(hexContent);
+
+            string hexString = swappedHexContent;
+            string formattedString = string.Join(",", Enumerable.Range(0, hexString.Length / 2)
+                .Select(i => "0x" + hexString.Substring(i * 2, 2)));
+
+            var formattedBuilder = new StringBuilder();
+            for (int i = 0; i < formattedString.Length; i += 80)
+            {
+                formattedBuilder.AppendLine(formattedString.Substring(i, Math.Min(80, formattedString.Length - i)));
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "TXT Files (*.txt)|*.txt";
+            saveFileDialog.InitialDirectory = Path.GetDirectoryName(bmpFilePath);
+            saveFileDialog.FileName = Path.GetFileNameWithoutExtension(bmpFilePath) + ".txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, formattedBuilder.ToString());
+            }
+        }
+
+        private string BmpToHex(string bmpFilePath)
+        {
+            using (var bmp = new Bitmap(bmpFilePath))
+            {
+                var bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, PixelFormat.Format16bppRgb565);
+                var bmpBytes = new byte[bmpData.Stride * bmpData.Height];
+                Marshal.Copy(bmpData.Scan0, bmpBytes, 0, bmpBytes.Length);
+                bmp.UnlockBits(bmpData);
+
+                // Convert byte array to HEX string
+                var hexBuilder = new StringBuilder(bmpBytes.Length*2);
+                foreach (byte b in bmpBytes)
+                {
+                    hexBuilder.AppendFormat("{0:X2}", b);
+                }
+                return hexBuilder.ToString();
+            }
+        }
+
+        private string SwapHexContent(string hexContent)
+        {
+            var swappedBuilder = new StringBuilder(hexContent.Length);
+            for (int i = 0; i < hexContent.Length; i += 4)
+            {
+                // Get the two bytes of the 16-bit segment
+                var byte1 = hexContent[i];
+                var byte2 = hexContent[i + 1];
+                var byte3 = hexContent[i + 2];
+                var byte4 = hexContent[i + 3];
+
+                // Swap the high and low bytes
+                swappedBuilder.Append(byte3);
+                swappedBuilder.Append(byte4);
+                swappedBuilder.Append(byte1);
+                swappedBuilder.Append(byte2);
+            }
+            return swappedBuilder.ToString();
+        }
+        private byte[] ImageToByteArray(Image image)
+    {
+         using (var ms = new MemoryStream())
+        {
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            return ms.ToArray();
+        }
+    }
+
+        private void btn_choose_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "BMP Files (*.bmp)|*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string bmpFilePath = openFileDialog.FileName;
+                text_path.Text = bmpFilePath;
+                pictureBox1.Load(bmpFilePath);
+                using (var fs = new FileStream(bmpFilePath, FileMode.Open, FileAccess.Read))
+                using (var br = new BinaryReader(fs))
+                {
+                    // Read BMP header
+                    var header = new byte[14];
+                    br.Read(header, 0, header.Length);
+
+                    // Read DIB header
+                    var dibHeader = new byte[40];
+                    br.Read(dibHeader, 0, dibHeader.Length);
+
+                    // Extract image width and height
+                    var width = BitConverter.ToInt32(dibHeader, 4);
+                    var height = BitConverter.ToInt32(dibHeader, 8);
+
+                    // Extract bit depth
+                    var bitDepth = BitConverter.ToInt16(dibHeader, 14);
+
+                    // Extract file size
+                    var fileSizeBytes = new FileInfo(bmpFilePath).Length;
+                    var fileSizeKB = fileSizeBytes / 1024.0;
+
+                    // Do something with bit depth and file size
+                    log.AppendText("图片大小" + width + "x" + height + "\r\n位深" + bitDepth + "\r\n文件大小" + fileSizeKB.ToString("0.00") + "KB");
+                }
+            }
+        }
+
+        private void ConcatenateHexFiles(string directoryPath)
+        {
+            string[] hexFilePaths = Directory.GetFiles(directoryPath, "*.bin");
+
+            Array.Sort(hexFilePaths);
+
+            string concatenatedHexFilePath = Path.Combine(directoryPath, "concatenated.bin");
+
+            using (var outputStream = new FileStream(concatenatedHexFilePath, FileMode.Create))
+            {
+                foreach (string hexFilePath in hexFilePaths)
+                {
+                    using (var inputStream = new FileStream(hexFilePath, FileMode.Open))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+
+                        while ((bytesRead = inputStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            outputStream.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show("Hex files concatenated successfully.");
+        }
+
+        private void btnConcatenate_Click(object sender, EventArgs e)
+        {
+            string directoryPath = Path.GetDirectoryName(Application.ExecutablePath);
+            ConcatenateHexFiles(directoryPath);
+        }
+
+        private void buttonhex_Click(object sender, EventArgs e)
+        {
+            string directoryPath = Path.GetDirectoryName(Application.ExecutablePath);
+            string[] bmpFilePaths = Directory.GetFiles(directoryPath, "*.bmp");
+
+            Array.Sort(bmpFilePaths);
+
+            foreach (string bmpFilePath in bmpFilePaths)
+            {
+                string hexContent = BmpToHex(bmpFilePath);
+                string swappedHexContent = SwapHexContent(hexContent);
+
+                string hexFilePath = Path.Combine(directoryPath, Path.GetFileNameWithoutExtension(bmpFilePath) + ".bin");
+                byte[] hexBytes = Enumerable.Range(0, swappedHexContent.Length / 2)
+                    .Select(i => Convert.ToByte(swappedHexContent.Substring(i * 2, 2), 16))
+                    .ToArray();
+
+                // 填充剩余的字节为0xFF
+                int paddingSize = 108 * 1024 - hexBytes.Length;
+                byte[] paddingBytes = Enumerable.Repeat((byte)0xFF, paddingSize).ToArray();
+
+                byte[] finalHexBytes = hexBytes.Concat(paddingBytes).ToArray();
+
+                File.WriteAllBytes(hexFilePath, finalHexBytes);
+            }
+
+            MessageBox.Show("图片转换成功。");
+        }
+    }
+}
